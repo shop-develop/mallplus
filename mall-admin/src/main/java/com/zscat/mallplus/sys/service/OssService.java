b770fcadfde1b84d979c3ba2130952ec02b0e5b1
shenzhuan <zhuan.shen@rjfittime.com>
2019-04-24 17:22:32 +0800

    上传模块修复

---
+++ mall-admin/src/main/java/com/zscat/mallplus/sys/service/OssService.java
@@ -0,0 +1,17 @@
+package com.zscat.mallplus.sys.service;
+
+
+import com.zscat.mallplus.sys.vo.OssCallbackResult;
+import com.zscat.mallplus.sys.vo.OssPolicyResult;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * oss上传管理Service
+ * https://gitee.com/zscat-platform/mall on 2018/5/17.
+ */
+public interface OssService {
+    OssPolicyResult policy();
+
+    OssCallbackResult callback(HttpServletRequest request);
+}
