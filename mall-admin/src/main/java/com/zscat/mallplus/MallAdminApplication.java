2556a49e764a57ac09760fc2de684485bdb2cf73
shenzhuan <zhuan.shen@rjfittime.com>
2019-04-27 13:52:00 +0800

    后台注册功能 验证码

--- mall-admin/src/main/java/com/zscat/mallplus/MallAdminApplication.java
+++ mall-admin/src/main/java/com/zscat/mallplus/MallAdminApplication.java
@@ -3,13 +3,14 @@ package com.zscat.mallplus;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * 应用启动入口
  * https://github.com/shenzhuan/mallplus on 2018/4/26.
  */
-@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 @MapperScan({"com.zscat.mallplus.mapper", "com.zscat.mallplus.ums.mapper", "com.zscat.mallplus.marking.mapper", "com.zscat.mallplus.cms.mapper", "com.zscat.mallplus.sys.mapper", "com.zscat.mallplus.oms.mapper", "com.zscat.mallplus.pms.mapper"})
 @EnableTransactionManagement
 public class MallAdminApplication {
